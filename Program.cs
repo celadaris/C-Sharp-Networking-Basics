4e1ebe1b85310a9fde8d46d7391c45d7720abcbd
celadaris <imnotkira13r@gmail.com>
2021-10-24 00:31:42 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDP_Async_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Network netCode = new Network();
+            netCode.SendData();
+        }
+    }
+
+    class Network
+    {
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 23000);
+        byte[] buffer = { 0, 1 };
+
+        public void SendData()
+        {
+            SocketAsyncEventArgs asocket = new SocketAsyncEventArgs();
+
+            try
+            {
+                asocket.SetBuffer(buffer, 0, buffer.Length);
+                asocket.RemoteEndPoint = ipep;
+                socket.SendToAsync(asocket);
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+                
+
+                asocket.Completed += DoneSending;
+            }
+            catch (Exception excp)
+            {
+
+                Console.WriteLine(excp);
+            }
+        }
+
+        private void DoneSending(object sender, SocketAsyncEventArgs e)
+        {
+            Console.WriteLine("Data sent..");
+        }
+    }
+}
